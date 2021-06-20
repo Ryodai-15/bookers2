3e1fd770582241d94c75fab5e49fc07e6652751d
Ryodai-15 <gon.4629.1208@gmail.com>
2021-06-20 05:32:31 +0000

    [Fix]bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/show'
   root to:'homes#top'
-  
+  devise_for :users
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+
+  resources :users,only: [:show,:edit,:update]
+
 end

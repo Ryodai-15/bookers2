9a7a549da9871e4f49da898d275687b2c2ecbfee
Ryodai-15 <gon.4629.1208@gmail.com>
2021-06-24 07:11:30 +0000

    [Finish]booker2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_081350) do
+ActiveRecord::Schema.define(version: 2021_06_24_070241) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2021_06_21_081350) do
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
     t.string "introduction"
-    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
